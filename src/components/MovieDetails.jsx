83563cdff1ed230c56a3b5342f1d7a8aef76ad38
nadav <nadavyona70@gmail.com>
2023-04-11 17:12:24 +0300

    d

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -6,6 +6,7 @@ import { AiFillStar } from 'react-icons/ai';
 
 
 
+
 const MovieDetails = () => {
   const [info, setInfo] = useState([])
   const [loading, setLoading] = useState(true)
@@ -36,14 +37,14 @@ const MovieDetails = () => {
               {<img style={{ borderRadius: "10%", boxShadow: "10px 9px 20px 3px black", height: "520px",marginTop:"80px",width:"320px",marginLeft:"25px"   }} src={info.Poster} alt="" />}
             </div>
             <div className='movie_info text-white ' style={{ fontFamily: "sans-serif", fontSize: "20px",paddingTop:"80px" }}>
-              <h1 style={{ fontWeight: "bold" }}>{info.Title}</h1>
+              <h1 style={{ fontWeight: "bold",borderBottom:"1px solid white" }}>{info.Title}</h1>
               <p>{info.Plot}</p>
-              <p style={{ fontWeight: "bold" }}>Genre:{info.Genre}</p>
+              <p>Genre:{info.Genre}</p>
               <p>Actors:{info.Actors}</p>
               <p>Released Date:{info.Released}</p>
               <p>Movie Length:{info.Runtime}</p>
               <p>Director:{info.Director}</p>
-              <p>Rating:{info.imdbRating}<AiFillStar style={{fontSize:"26px",color:"#f5c518"}}/></p>
+              <p style={{fontWeight: "bold"}} >Rating:{info.imdbRating} <AiFillStar style={{fontSize:"26px",color:"#f5c518"}}/></p>
               <div className='mt-3'>
                 <div onClick={() => nav(-1)} style={{ fontWeight: '600' }} className='btn  btn-info'>Back</div>
 
